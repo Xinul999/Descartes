8377b382872a7edf116381011964571a839ae523
HachimiBouizegarene <hachimi.bouizegarene@outlook.fr>
2023-10-09 18:55:02 +0200

    No comment

---
+++ src/main/java/org/hachimi/EduCat/controller/Home.java
@@ -0,0 +1,13 @@
+package org.hachimi.EduCat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+//    @GetMapping(path = "/")
+//    public String welcome(){
+//        return "Hello world !";
+//    }
+}
