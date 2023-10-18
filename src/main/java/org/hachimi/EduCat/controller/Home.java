005c892f9eebb11c54b0bf7dda39e965e5ef4b9f
HachimiBouizegarene <hachimi.bouizegarene@outlook.fr>
2023-10-18 09:19:54 +0200

    ajout de /login

--- src/main/java/org/hachimi/EduCat/controller/Home.java
+++ src/main/java/org/hachimi/EduCat/controller/Home.java
@@ -6,8 +6,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -19,10 +18,24 @@ public class Home {
     public Home(DataService dataService){
         this.dataService = dataService;
     }
-    @GetMapping(path = "/data")
-    public String welcome(){
-        List<Map<String, Object>> data = dataService.importerDonnees();
-        return new JSONArray(data).toString();
+//    @GetMapping(path = "/data")
+//    public String welcome(){
+//        JSONArray data = dataService.getData("infos");
+//        return data.toString();
+//    }
+
+    @PostMapping(path = "/login")
+    public String login(@RequestBody String body){
+        JSONObject json = new JSONObject(body);
+        String user_name = json.getString("user_name");
+        String password = json.getString("password");
+        json = dataService.getUser(user_name, password);
+        return json.toString();
+    }
+
+    @GetMapping(path = "/test")
+    public String test(){
+        return "GOAT";
     }
 
 
--- src/main/java/org/hachimi/EduCat/service/DataService.java
+++ src/main/java/org/hachimi/EduCat/service/DataService.java
@@ -1,20 +1,85 @@
 package org.hachimi.EduCat.service;
 
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import java.sql.*;
 
 @Service
 public class DataService {
-    private final JdbcTemplate jdbcTemplate;
-    public DataService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+
+    private String databaseUrl;
+    private String databasePassword;
+
+    private String databaseUserName;
+
+    Connection connection = null;
+    public DataService( @Value("${database.url}") String databaseUrl,
+                        @Value("${database.username}") String databaseUserName,
+                        @Value("${database.password}") String databasePassword) {
+
+        this.databaseUrl = databaseUrl;
+        this.databaseUserName = databaseUserName;
+        this.databasePassword = databasePassword;
+        try{
+            connection = dataBaseConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
-    public List<Map<String, Object>> importerDonnees(){
-        String sql = "SELECT * FROM infos";
-        return  jdbcTemplate.queryForList(sql);
+    @Bean
+    public Connection dataBaseConnection() throws SQLException{
+        return DriverManager.getConnection(databaseUrl, databaseUserName, databasePassword);
     }
+
+    public JSONObject getUser(String user_name, String password){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM utilisateur WHERE Nom = '"+user_name + "' AND MDP ='" + password  + "'");
+            if(resultSet.next()){
+                ResultSetMetaData metaData = resultSet.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    jsonObject.put(columnName, resultSet.getString(columnName));
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    public JSONArray getTableData(String table) {
+        JSONArray jsonArray =  new JSONArray();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM " + table);
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            while (resultSet.next()) {
+                JSONObject json = new JSONObject();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    json.put(columnName, resultSet.getString(columnName));
+                }
+                jsonArray.put(json);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+    }
+
+//    public List<Map<String, Object>> getUser(string ){
+//        String sql = "SELECT * FROM infos";
+//        return  jdbcTemplate.queryForList(sql);
+//    }
 }
